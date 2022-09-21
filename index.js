30d71e3ad39fb6452bb59c3670ef7c7ba4bf335f
Mariia <m.goncharenkoo@icloud.com>
2022-09-21 13:52:04 -0400

    commit tree 3d03a37da82d035949d99265499b41dd61ff96ca

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (typeof activity === 'undefined') {
+        return "This Saturday, I want to roller-skate!"
+    }
+    else {
+        return `This Saturday, I want to ${activity}!`
+    }
+}
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
